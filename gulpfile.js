6851705447a2108afaa9c81765c7bbc28ae6efa9
jenkka <jcglez98@gmail.com>
2021-09-29 18:48:57 -0500

    Testing

--- gulpfile.js
+++ gulpfile.js
@@ -26,11 +26,11 @@ gulp.task('styles:watch', gulp.series('styles', function(done) {
 }));
 
 gulp.task('serve', function(done) {
-    connect.server({
-        root: [your_project_path],
-        port: process.env.PORT || 5000, // localhost:5000
-        livereload: false
-    });
+    // connect.server({
+    //     root: [your_project_path],
+    //     port: process.env.PORT || 5000, // localhost:5000
+    //     livereload: false
+    // });
 
     const allTask = gulp.parallel('styles:watch', 'scripts:watch');
     allTask();
@@ -38,10 +38,10 @@ gulp.task('serve', function(done) {
 });
 
 gulp.task('default', gulp.parallel('styles', 'scripts', function(done) {
-    connect.server({
-        root: [your_project_path],
-        port: process.env.PORT || 5000, // localhost:5000
-        livereload: false
-    });
+    // connect.server({
+    //     root: [your_project_path],
+    //     port: process.env.PORT || 5000, // localhost:5000
+    //     livereload: false
+    // });
     done();
 }));
